b55cd1ffa618687483a6ec8f0fa77171840c2b66
Flynn <65683857+flynnwasnt@users.noreply.github.com>
2020-07-18 19:42:57 +0100

    Added a script to restart instead of shutdown

---
+++ rvm.sh
@@ -0,0 +1,59 @@
+
+#!/bin/bash
+
+echo "Script Started"
+
+vm=""
+waitingforshutdown=false
+activevm=""
+macOS=""
+Windows=""
+
+tmp=$(virsh list --all | grep "macOS" | awk '{ print $3}')
+if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
+then
+   macOS=off
+else
+    vm="on"
+    activevm="macOS"
+fi
+
+
+tmp=$(virsh list --all | grep "Windows" | awk '{ print $3}')
+if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
+then
+   Windows="off"
+else
+    vm="on"
+    activevm="Windows"
+fi
+
+
+if [ "$macOS" = "off" ] && [ "$Windows" = "off" ]
+then
+	echo "No VMs are running. Exiting."
+	exit
+fi
+
+
+
+if vm="on"
+then	
+	echo "$activevm is running, entering loop."
+fi
+
+while vm="on"
+do
+	tmp=$(virsh list --all | grep "$activevm" | awk '{ print $3}')
+	if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
+	then
+	    vm="off"
+	    echo "$activevm has shutdown! Restarting host in 5 seconds."
+	    sleep 5
+	    sudo restart now
+	    exit
+	else
+	    vm="on"
+	fi
+done
+
