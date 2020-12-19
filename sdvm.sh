aa83b0641a7ce5ed194dfaaee76e91086d6574db
Flynn <65683857+flynnwasnt@users.noreply.github.com>
2020-12-19 18:24:02 +0000

    Refactored sdvm.sh
    Rewrite of entire script. Adds reboot option, now works with any virtual machine instead of those with specific names, easy to use UI.

--- sdvm.sh
+++ sdvm.sh
@@ -1,59 +1,90 @@
-
 #!/bin/bash
 
-echo "Script Started"
+echo "Once VM is shutdown, would you like to..."
+echo "1. Shutdown"
+echo "2. Reboot"
+# echo "3. Boot Windows"
+# echo "4. Boot macOS"
+# echo "5. Boot Ubuntu"
+read -s -n 1 prompt
+printf "\n"
 
-vm=""
-waitingforshutdown=false
-activevm=""
-macOS=""
-Windows=""
-
-tmp=$(virsh list --all | grep "macOS" | awk '{ print $3}')
+tmp=$(virsh list --all | grep "running" | awk '{ print $3}')
 if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
 then
-   macOS=off
+	vm="off"
 else
-    vm="on"
-    activevm="macOS"
+	vm="on"
 fi
 
-
-tmp=$(virsh list --all | grep "Windows" | awk '{ print $3}')
-if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
-then
-   Windows="off"
-else
-    vm="on"
-    activevm="Windows"
-fi
-
-
-if [ "$macOS" = "off" ] && [ "$Windows" = "off" ]
+if [ "$vm" = "off" ]
 then
 	echo "No VMs are running. Exiting."
 	exit
 fi
 
-
-
 if vm="on"
 then	
-	echo "$activevm is running, entering loop."
+	echo "At least one VM is running, entering loop."
 fi
 
 while vm="on"
 do
-	tmp=$(virsh list --all | grep "$activevm" | awk '{ print $3}')
+	tmp=$(virsh list --all | grep "running" | awk '{ print $3}')
 	if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ])
 	then
 	    vm="off"
-	    echo "$activevm has shutdown! Shutting down host in 5 seconds."
-	    sleep 5
-	    shutdown now
-	    exit
+		echo "The VM has shutdown! Waiting five seconds before continuing..."
+		sleep 5
+		break
 	else
 	    vm="on"
 	fi
 done
 
+# resetbugcheck=$("sudo virsh start Ubuntu | grep '127' | awk '{ print $3}'" > /dev/null 2>&1)
+# if ([ "x$resetbugcheck" == "x" ] || [ "x$resetbugcheck" != "xrunning" ])
+# then
+# 	echo "Reset bug detected :("
+# 	echo "Rebooting host now."
+# 	sudo reboot now
+# 	exit
+# else
+# 	resetbug=False
+# 	exit
+# fi
+
+if [ "$prompt" = "1" ] && [ "$vm" = "off" ]
+then
+	echo "Shutting down host now."
+    sudo shutdown now
+    exit
+fi
+
+if [ "$prompt" = "2" ] && [ "$vm" = "off" ]
+then
+    echo "Rebooting host now."
+	sudo reboot now
+	exit
+fi
+
+# if [ "$prompt" = "3" ] && [ "$vm" = "off" ]
+# then
+# 	echo "Booting Windows now."
+# 	sudo virsh start Windows
+# 	exit
+# fi
+
+# if [ "$prompt" = "4" ] && [ "$vm" = "off" ]
+# then
+# 	echo "Booting macOS now."
+# 	sudo virsh start macOS
+# 	exit
+# fi
+
+# if [ "$prompt" = "5" ] && [ "$vm" = "off" ]
+# then
+# 	echo "Booting Ubuntu now."
+# 	sudo virsh start Ubuntu
+# 	exit
+# fi
